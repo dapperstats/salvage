7a7b55ca296de2ebd33807ff698b480a503c5dbb
Juniper Simonis <dapperstats@gmail.com>
2020-01-12 20:00:45 -0800

    Update update_github_data.bash

--- scripts/update_github_data.bash
+++ scripts/update_github_data.bash
@@ -9,11 +9,10 @@ git checkout master
 git add data/* 
 git commit -m "updating_data"
 
-git remote add deploy https://${GITHUB_TOKEN}@github.com/dapperstats/salvage.git > /dev/null 2>&1
+git remote add deploy https://${GITHUB_DEPLOY_PAT}@github.com/dapperstats/salvage.git > /dev/null 2>&1
 
 current_date=`date -I | head -c 10`
 git tag $current_date
 
 git push --quiet deploy master > /dev/null 2>&1
 git push --quiet deploy --tags > /dev/null 2>&1
-curl -v -i -X POST -H "Content-Type:application/json" -H "Authorization: token $GITHUB_RELEASE_TOKEN" https://api.github.com/repos/dapperstats/salvage/releases -d "{\"tag_name\":\"$current_date\"}"
