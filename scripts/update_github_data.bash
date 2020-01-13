d290886f66215a8c0ce5da40d631cb4f9c93088b
Juniper Simonis <dapperstats@gmail.com>
2020-01-12 22:23:46 -0800

    keeping on working on

--- scripts/update_github_data.bash
+++ scripts/update_github_data.bash
@@ -12,7 +12,7 @@ git commit -m "updating_data Travis job: $TRAVIS_JOB_NUMBER"
 
 git remote add deploy https://${GITHUB_DEPLOY_PAT}@github.com/dapperstats/salvage.git > /dev/null 2>&1
 
-git tag $TRAVIS_BUILD_NUMBER
+git tag $TRAVIS_JOB_NUMBER
 git push --quiet --set-upstream deploy master
 git push --quiet deploy --tags > /dev/null 2>&1
 curl -v -i -X POST -H "Content-Type:application/json" -H "Authorization: token $GITHUB_RELEASE_PAT" https://api.github.com/repos/dapperstats/salvage/releases -d "{\"tag_name\":\"$TRAVIS_JOB_NUMBER\"}"
