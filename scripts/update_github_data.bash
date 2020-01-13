600cf8b6ee3d71f00f56f213838bcfb5a885bb0c
Juniper Simonis <dapperstats@gmail.com>
2020-01-12 21:01:09 -0800

    Update update_github_data.bash

--- scripts/update_github_data.bash
+++ scripts/update_github_data.bash
@@ -5,14 +5,14 @@
 git config --global user.email "deploy@dapperstats.com"
 git config --global user.name "DAPPER Deploy Bot"
 
+
 git checkout master
 git add data/*.csv
-git commit -m "updating_data Travis build: $TRAVIS_BUILD_BUMBER"
+git commit -m "updating_data Travis build: $TRAVIS_BUILD_NUMBER"
 
 git remote add deploy https://${GITHUB_DEPLOY_PAT}@github.com/dapperstats/salvage.git > /dev/null 2>&1
 
+git tag $TRAVIS_BUILD_NUMBER
 git push --quiet --set-upstream deploy master
-
-# git push --quiet deploy master > /dev/null 2>&1
-# git push --quiet deploy --tags > /dev/null 2>&1
-# curl -v -i -X POST -H "Content-Type:application/json" -H "Authorization: token $GITHUB_RELEASE_PAT" https://api.github.com/repos/dapperstats/salvage/releases -d "{\"tag_name\":\"$current_date\"}"
+git push --quiet deploy --tags > /dev/null 2>&1
+# curl -v -i -X POST -H "Content-Type:application/json" -H "Authorization: token $GITHUB_RELEASE_PAT" https://api.github.com/repos/dapperstats/salvage/releases -d "{\"tag_name\":\"$TRAVIS_BUILD_NUMBER\"}"
