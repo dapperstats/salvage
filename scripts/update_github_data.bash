b32b195f3126455747676ab5bb4c934fb77a2d8a
Juniper Simonis <dapperstats@gmail.com>
2020-01-12 20:09:02 -0800

    Update update_github_data.bash

--- scripts/update_github_data.bash
+++ scripts/update_github_data.bash
@@ -16,3 +16,4 @@ git tag $current_date
 
 git push --quiet deploy master > /dev/null 2>&1
 git push --quiet deploy --tags > /dev/null 2>&1
+curl -v -i -X POST -H "Content-Type:application/json" -H "Authorization: token $GITHUB_RELEASE_PAT" https://api.github.com/repos/dapperstats/salvage/releases -d "{\"tag_name\":\"$current_date\"}"
