7a8c0e5031abb5f749d7014bfc93225ecf7205a0
Juniper Simonis <dapperstats@gmail.com>
2020-01-24 16:17:36 -0800

    resource

--- scripts/salvage_script.R
+++ scripts/salvage_script.R
@@ -1,6 +1,8 @@
+source("scripts/r_functions.R")
+source("scripts/salvage_functions.R")
 salvage <- read_database()
 
-most_recent_samples(database)
+most_recent_samples(salvage)
 dates <- seq.Date(as.Date("2020-01-01"), as.Date("2020-01-22"), 1)
-daily_volume(database, dates)
-daily_counts(database, dates)
+daily_volume(salvage, dates)
+daily_counts(salvage, dates)
