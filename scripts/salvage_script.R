bd5dfc92769acfe2dc6a424bcf046cdde14b8551
Juniper Simonis <dapperstats@gmail.com>
2020-01-26 15:03:43 -0800

    Update salvage_script.R

--- scripts/salvage_script.R
+++ scripts/salvage_script.R
@@ -11,7 +11,7 @@ plot(as.Date(x$Date), x$Sample_Volume/x$Pumping_Volume)
 most_recent <- most_recent_samples(salvage)
 most_recent <- data.frame(t(most_recent))
 mr_file <- paste0("site/static/files/most_recent_samples.csv")
-write.csv(most_recent, file = mr_file, row.names = FALSE)
-
+#write.csv(most_recent, file = mr_file, row.names = FALSE)
+list.files("site/static", recursive=TRUE)
 message("completed")
 
