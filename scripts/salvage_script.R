4992480e751fa1a04416dcd319d312bc18f831d0
Juniper Simonis <dapperstats@gmail.com>
2020-01-26 15:50:50 -0800

    json

--- scripts/salvage_script.R
+++ scripts/salvage_script.R
@@ -4,14 +4,11 @@ salvage <- read_database()
 
 most_recent_samples(salvage)
 dates <- seq.Date(as.Date("2020-01-01"), as.Date("2020-01-22"), 1)
-daily_volume(salvage, dates)
 x <- daily_sample_vols(salvage, dates)
-plot(as.Date(x$Date), x$Sample_Volume/x$Pumping_Volume)
 
 most_recent <- most_recent_samples(salvage)
 most_recent <- data.frame(t(most_recent))
 mr_file <- "files/most_recent_samples.csv"
 write.csv(most_recent, file = mr_file, row.names = FALSE)
-list.files()
 message("completed")
 
