b7a21a8019d938fefbb9ba60d3b6ce813021bad2
Juniper Simonis <dapperstats@gmail.com>
2020-01-26 23:37:16 -0800

    Update salvage_script.R

--- scripts/salvage_script.R
+++ scripts/salvage_script.R
@@ -8,7 +8,7 @@ sample_vols <- daily_sample_vols(salvage, dates)
 sv_table_file <- "files/sample_vols.csv"
 write.csv(sample_vols, file = sv_table_file, row.names = FALSE)
 sample_vol_fig(sample_vols)
-
+message("hello")
 most_recent <- most_recent_samples(salvage)
 most_recent <- data.frame(t(most_recent))
 mr_file <- "files/most_recent_samples.csv"
