25479ffb732d578367884e620fcac14738a25d45
Juniper Simonis <dapperstats@gmail.com>
2020-01-27 18:50:15 -0800

    try

--- scripts/salvage_script.R
+++ scripts/salvage_script.R
@@ -2,18 +2,20 @@ source("scripts/r_functions.R")
 source("scripts/salvage_functions.R")
 salvage <- read_database()
 
+dir.create("site/files", showWarnings = FALSE)
 most_recent_samples(salvage)
 dates <- seq.Date(as.Date("2020-01-01"), as.Date("2020-01-22"), 1)
 sample_vols <- daily_sample_vols(salvage, dates)
-sv_table_file <- "site/docs/sample_vols.csv"
+sv_table_file <- "site/files/sample_vols.csv"
 write.csv(sample_vols, file = sv_table_file, row.names = FALSE)
 exported_volumes_fig(sample_vols)
 message("hello")
 most_recent <- most_recent_samples(salvage)
 most_recent <- data.frame(t(most_recent))
-mr_file <- "site/docs/most_recent_samples.csv"
+mr_file <- "site/files/most_recent_samples.csv"
 write.csv(most_recent, file = mr_file, row.names = FALSE)
 
 rmarkdown::render_site("site")
+
 message("completed")
 
