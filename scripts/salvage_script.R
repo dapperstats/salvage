2356c54974df36046daa10406b61ac787730464b
Juniper Simonis <dapperstats@gmail.com>
2020-01-28 23:35:34 -0800

    0-11-0
    ack caught that placeholder i missed, hance why it was stuck on 1/22

--- scripts/salvage_script.R
+++ scripts/salvage_script.R
@@ -3,8 +3,8 @@ source("scripts/salvage_functions.R")
 salvage <- read_database()
 
 dir.create("site/files", showWarnings = FALSE)
-most_recent_samples(salvage)
-dates <- seq.Date(as.Date("2020-01-01"), as.Date("2020-01-22"), 1)
+max_date <- max(as.Date(most_recent_samples(salvage)))
+dates <- seq.Date(as.Date("2020-01-01"), max_date, 1)
 sample_vols <- daily_sample_vols(salvage, dates)
 sv_table_file <- "site/files/sample_vols.csv"
 write.csv(sample_vols, file = sv_table_file, row.names = FALSE)
